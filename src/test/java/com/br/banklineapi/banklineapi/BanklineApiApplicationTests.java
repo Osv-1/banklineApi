fdbe410ac0a904914e8c027aeaab1b24abfaf7f9
Takeo <oswaldotakeo1@gmail.com>
2022-05-09 17:12:07 -0300

    Initial commit

---
+++ src/test/java/com/br/banklineapi/banklineapi/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.banklineapi.banklineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
